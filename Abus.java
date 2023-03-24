56294be0b0b498bf0a6530662c1fc8c954fa5b1b
Chhay2 <117246850+Chhay2@users.noreply.github.com>
2023-03-24 21:19:02 +0700

    Add files via upload

---
+++ Abus.java
@@ -0,0 +1,13 @@
+public class Abus {
+    public static void main(String[] args) {
+        System.out.println("""
+            This website is about to test your typing skill by measuring your typing speed and accuracy.
+            This website was developed by group of student that have collaborated with each other to complete this task. 
+            we would like to honor: 
+            1. Tim Tito that has responsibled on developing registeration page 
+            2. Veasna Dara that has responsibled on writing procedure of the website
+            3. Lim Techchhiv that has been designed Landing page and login page.
+                
+                """);
+    }
+}
